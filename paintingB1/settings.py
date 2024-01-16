7a90ff52f6e79fa446f4f76f12bb2ad5b39fdbce
phamminh12 <aeonepiece7a4@gmail.com>
2024-01-17 00:50:06 +0700

    fix allowed_hosts

--- paintingB1/settings.py
+++ paintingB1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k@ww+4m9(70pibsqf74jsfknn88fvqf43-(!qrz9!k141m5%t-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['art-one.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['art-one.onrender.com', 'localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "https://art-one.onrender.com",
     'localhost', '127.0.0.1'
